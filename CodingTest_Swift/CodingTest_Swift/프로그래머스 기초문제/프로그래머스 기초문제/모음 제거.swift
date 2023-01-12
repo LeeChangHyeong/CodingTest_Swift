795b19124cb3fa2e5acfaefc587c0aacd031c69a
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2023-01-12 20:37:07 +0900

    [Clear] 모음제거 - 2022/01/12

---
+++ CodingTest_Swift/CodingTest_Swift/프로그래머스 기초문제/프로그래머스 기초문제/모음 제거.swift
@@ -0,0 +1,21 @@
+//
+//  모음 제거.swift
+//  CodingTest_Swift
+//
+//  Created by 이창형 on 2023/01/12.
+//
+
+//import Foundation
+//
+//func solution(_ my_string:String) -> String {
+//    var answer = ""
+//    
+//    for i in my_string {
+//        if i == "a" || i == "e" || i == "i" || i == "o" || i == "u" {
+//            
+//        } else {
+//            answer.append(i)
+//        }
+//    }
+//    return answer
+//}
