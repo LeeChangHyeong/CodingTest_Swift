27248a86636bc132de98a5c1554f2d23269f2662
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2023-01-12 21:52:07 +0900

    [Clear] 숨어있는 숫자의 덧셈(1) - 2022/01/12

--- CodingTest_Swift/CodingTest_Swift/프로그래머스 기초문제/프로그래머스 기초문제/문자열 정렬하기(1).swift
+++ CodingTest_Swift/CodingTest_Swift/프로그래머스 기초문제/프로그래머스 기초문제/문자열 정렬하기(1).swift
@@ -5,16 +5,16 @@
 //  Created by 이창형 on 2023/01/12.
 //
 
-import Foundation
-
-func solution(_ my_string:String) -> [Int] {
-    var answer = [Int]()
-    
-    for i in my_string {
-        guard let num = Int(String(i)) else { continue }
-        print(num)
-        answer.append(num)
-    }
-    
-    return answer.sorted()
-}
+//import Foundation
+//
+//func solution(_ my_string:String) -> [Int] {
+//    var answer = [Int]()
+//    
+//    for i in my_string {
+//        guard let num = Int(String(i)) else { continue }
+//        print(num)
+//        answer.append(num)
+//    }
+//    
+//    return answer.sorted()
+//}
---
+++ CodingTest_Swift/CodingTest_Swift/프로그래머스 기초문제/프로그래머스 기초문제/숨어있는 숫자의 덧셈(1).swift
@@ -0,0 +1,19 @@
+//
+//  숨어있는 숫자의 덧셈(1).swift
+//  CodingTest_Swift
+//
+//  Created by 이창형 on 2023/01/12.
+//
+
+import Foundation
+
+func solution(_ my_string:String) -> Int {
+    var answer = 0
+    
+    for i in my_string {
+        guard let num = Int(String(i)) else { continue }
+        answer += num
+    }
+    
+    return answer
+}
