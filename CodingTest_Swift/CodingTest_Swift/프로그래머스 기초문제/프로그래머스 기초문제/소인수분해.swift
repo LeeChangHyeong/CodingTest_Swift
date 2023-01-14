3f7ce283bd43a361cb71d6a62319f3e378057696
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2023-01-14 13:27:36 +0900

    [Cleat] 소인수분해 - 2022/01/14

---
+++ CodingTest_Swift/CodingTest_Swift/프로그래머스 기초문제/프로그래머스 기초문제/소인수분해.swift
@@ -0,0 +1,25 @@
+//
+//  소인수분해.swift
+//  CodingTest_Swift
+//
+//  Created by 이창형 on 2023/01/14.
+//
+
+import Foundation
+
+func solution(_ n:Int) -> [Int] {
+    var answer: Set<Int> = []
+    var count = 2
+    var num = n
+    
+    while num != 1 {
+        if num % count == 0 {
+            answer.insert(count)
+            num = num / count
+            count = 2
+        } else {
+            count += 1
+        }
+    }
+    return Array(answer.sorted())
+}
