37051140bd76083fbf23baf2c971082bee998333
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2023-01-14 16:45:15 +0900

    [Clear] 컨트롤 제트 - 2022/01/14

---
+++ CodingTest_Swift/CodingTest_Swift/프로그래머스 기초문제/프로그래머스 기초문제/중복된 문자 제거.swift
@@ -0,0 +1,21 @@
+//
+//  중복된 문자 제거.swift
+//  CodingTest_Swift
+//
+//  Created by 이창형 on 2023/01/14.
+//
+
+//import Foundation
+//
+//func solution(_ my_string:String) -> String {
+//    var result = ""
+//    
+//    for c in my_string {
+//        // result에 c가 없으면
+//        if !result.contains(c) {
+//            // result에 c추가
+//            result += String(c)
+//        }
+//    }
+//    return result
+//}
--- CodingTest_Swift/CodingTest_Swift/프로그래머스 기초문제/프로그래머스 기초문제/컨트롤 제트.swift
+++ CodingTest_Swift/CodingTest_Swift/프로그래머스 기초문제/프로그래머스 기초문제/컨트롤 제트.swift
@@ -5,21 +5,21 @@
 //  Created by 이창형 on 2023/01/14.
 //
 
-import Foundation
-
-func solution(_ s:String) -> Int {
-    // split을 사용하면 separator에 적은 것을 기준으로 나눠 저장한다
-    var a = s.split(separator: " ")
-    var result = 0
-    
-    // enumerated는 배열을 읽어온다 (i는 index, j는 값)
-    for (i,j) in a.enumerated() {
-        if j == "Z" {
-            result -= Int(String(a[i - 1]))!
-            
-        } else {
-            result += Int(j)!
-        }
-    }
-    return result
-}
+//import Foundation
+//
+//func solution(_ s:String) -> Int {
+//    // split을 사용하면 separator에 적은 것을 기준으로 나눠 저장한다
+//    var a = s.split(separator: " ")
+//    var result = 0
+//    
+//    // enumerated는 배열을 읽어온다 (i는 index, j는 값)
+//    for (i,j) in a.enumerated() {
+//        if j == "Z" {
+//            result -= Int(String(a[i - 1]))!
+//            
+//        } else {
+//            result += Int(j)!
+//        }
+//    }
+//    return result
+//}
