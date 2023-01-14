11f3f9d866faf28735d5e774fc97007012340f1e
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2023-01-14 16:50:29 +0900

    [Clear] 세균증식 - 2022/01/14

---
+++ CodingTest_Swift/CodingTest_Swift/프로그래머스 기초문제/프로그래머스 기초문제/세균 증식.swift
@@ -0,0 +1,21 @@
+//
+//  세균 증식.swift
+//  CodingTest_Swift
+//
+//  Created by 이창형 on 2023/01/14.
+//
+
+//import Foundation
+//
+//func solution(_ n:Int, _ t:Int) -> Int {
+//    var hour = 0
+//    var answer = n
+//    
+//    while hour != t {
+//        answer *= 2
+//        hour += 1
+//    }
+//    
+//    
+//    return answer
+//}
