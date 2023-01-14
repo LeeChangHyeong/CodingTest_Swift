99c8315d202a97c032e99b65f86953b06399f4a3
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2023-01-14 13:50:18 +0900

    [Clear] 컨트롤 제트 - 2022/01/14

--- CodingTest_Swift/CodingTest_Swift/프로그래머스 기초문제/프로그래머스 기초문제/배열 자르기.swift
+++ CodingTest_Swift/CodingTest_Swift/프로그래머스 기초문제/프로그래머스 기초문제/배열 자르기.swift
@@ -5,14 +5,14 @@
 //  Created by 이창형 on 2023/01/09.
 //
 
-import Foundation
-
-func solution(_ numbers:[Int], _ num1:Int, _ num2:Int) -> [Int] {
-    var answer = [Int]()
-    
-    for i in num1...num2 {
-        answer.append(numbers[i])
-    }
-    
-    return answer
-}
+//import Foundation
+//
+//func solution(_ numbers:[Int], _ num1:Int, _ num2:Int) -> [Int] {
+//    var answer = [Int]()
+//    
+//    for i in num1...num2 {
+//        answer.append(numbers[i])
+//    }
+//    
+//    return answer
+//}
--- CodingTest_Swift/CodingTest_Swift/프로그래머스 기초문제/프로그래머스 기초문제/숨어있는 숫자의 덧셈(1).swift
+++ CodingTest_Swift/CodingTest_Swift/프로그래머스 기초문제/프로그래머스 기초문제/숨어있는 숫자의 덧셈(1).swift
@@ -5,15 +5,15 @@
 //  Created by 이창형 on 2023/01/12.
 //
 
-import Foundation
-
-func solution(_ my_string:String) -> Int {
-    var answer = 0
-    
-    for i in my_string {
-        guard let num = Int(String(i)) else { continue }
-        answer += num
-    }
-    
-    return answer
-}
+//import Foundation
+//
+//func solution(_ my_string:String) -> Int {
+//    var answer = 0
+//    
+//    for i in my_string {
+//        guard let num = Int(String(i)) else { continue }
+//        answer += num
+//    }
+//    
+//    return answer
+//}
