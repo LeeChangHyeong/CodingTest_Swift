26841781631898f66c776599ee8bdafe10b490a8
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2023-01-14 18:14:52 +0900

    [Clear] n의 배수 고르기 - 2022/01/14

---
+++ CodingTest_Swift/CodingTest_Swift/프로그래머스 기초문제/프로그래머스 기초문제/n의 배수 고르기.swift
@@ -0,0 +1,13 @@
+//
+//  n의 배수 고르기.swift
+//  CodingTest_Swift
+//
+//  Created by 이창형 on 2023/01/14.
+//
+
+import Foundation
+
+func solution(_ n:Int, _ numlist:[Int]) -> [Int] {
+    let answer = numlist.filter { $0 % n == 0 }
+    return answer
+}
