3b7f31889bfca5e84ccb8a8f26c4c2d1948c0cef
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2023-01-17 14:48:19 +0900

    [Clear] 369게임 - 2023/01/17

---
+++ CodingTest_Swift/CodingTest_Swift/프로그래머스 기초문제/프로그래머스 기초문제/369게임.swift
@@ -0,0 +1,18 @@
+//
+//  369게임.swift
+//  CodingTest_Swift
+//
+//  Created by 이창형 on 2023/01/17.
+//
+
+import Foundation
+
+func solution(_ order:Int) -> Int {
+    var count = 0
+    for i in String(order) {
+        if Int(String(i)) == 3 || Int(String(i)) == 6 || Int(String(i)) == 9 {
+            count += 1
+        }
+    }
+    return count
+}
--- CodingTest_Swift/CodingTest_Swift/프로그래머스 기초문제/프로그래머스 기초문제/가까운 수.swift
+++ CodingTest_Swift/CodingTest_Swift/프로그래머스 기초문제/프로그래머스 기초문제/가까운 수.swift
@@ -5,24 +5,24 @@
 //  Created by 이창형 on 2023/01/17.
 //
 
-import Foundation
-
-func solution(_ array:[Int], _ n:Int) -> Int {
-    var num = 100
-    var answer = 0
-    
-    for i in array.sorted() {
-        if n - i < 0 {
-            if -(n - i) < num {
-                num = -(n - i)
-                answer = i
-            }
-        } else {
-            if (n - i) < num {
-                num = (n - i)
-                answer = i
-            }
-        }
-    }
-    return answer
-}
+//import Foundation
+//
+//func solution(_ array:[Int], _ n:Int) -> Int {
+//    var num = 100
+//    var answer = 0
+//    
+//    for i in array.sorted() {
+//        if n - i < 0 {
+//            if -(n - i) < num {
+//                num = -(n - i)
+//                answer = i
+//            }
+//        } else {
+//            if (n - i) < num {
+//                num = (n - i)
+//                answer = i
+//            }
+//        }
+//    }
+//    return answer
+//}
