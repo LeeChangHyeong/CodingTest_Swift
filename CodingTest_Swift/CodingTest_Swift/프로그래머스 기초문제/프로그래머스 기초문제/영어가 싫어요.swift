cc36414e9983e27b6e674a060672de5312ccd67c
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2023-01-17 16:43:25 +0900

    [Clear] 인덱스 바꾸기 - 2023/01/17

---
+++ CodingTest_Swift/CodingTest_Swift/프로그래머스 기초문제/프로그래머스 기초문제/영어가 싫어요.swift
@@ -0,0 +1,20 @@
+//
+//  영어가 싫어요.swift
+//  CodingTest_Swift
+//
+//  Created by 이창형 on 2023/01/17.
+//
+
+//import Foundation
+//
+//func solution(_ numbers:String) -> Int64 {
+//    var englishArr: [String: String] = ["zero": "0","one": "1","two": "2","three": "3","four": "4","five": "5","six": "6","seven": "7","eight": "8","nine": "9"]
+//    var answer = numbers
+//
+//    for i in englishArr {
+//        // replacingOccurrences를 사용하여 영어(of)를 숫자(with)로 바꾼다
+//        answer = answer.replacingOccurrences(of: i.key, with: i.value)
+//    }
+//
+//    return Int64(answer)!
+//}
---
+++ CodingTest_Swift/CodingTest_Swift/프로그래머스 기초문제/프로그래머스 기초문제/인덱스 바꾸기.swift
@@ -0,0 +1,25 @@
+//
+//  인덱스 바꾸기.swift
+//  CodingTest_Swift
+//
+//  Created by 이창형 on 2023/01/17.
+//
+
+//import Foundation
+//
+//func solution(_ my_string:String, _ num1:Int, _ num2:Int) -> String {
+//    var stringArray = [String]()
+//    var answer = ""
+//    
+//    for i in my_string {
+//        stringArray.append(String(i))
+//    }
+//    
+//    stringArray.swapAt(num1, num2)
+//    
+//    for i in stringArray {
+//        answer.append(i)
+//    }
+//
+//    return answer
+//}
