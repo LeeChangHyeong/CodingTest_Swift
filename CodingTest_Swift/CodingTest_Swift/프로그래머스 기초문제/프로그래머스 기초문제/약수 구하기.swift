c71d6b87e02869af173e9481379e94ede3403b0c
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2023-01-17 17:15:44 +0900

    [Clear] 약수 구하기 - 2023/01/17

---
+++ CodingTest_Swift/CodingTest_Swift/프로그래머스 기초문제/프로그래머스 기초문제/약수 구하기.swift
@@ -0,0 +1,20 @@
+//
+//  약수 구하기.swift
+//  CodingTest_Swift
+//
+//  Created by 이창형 on 2023/01/17.
+//
+
+//import Foundation
+//
+//func solution(_ n:Int) -> [Int] {
+//    var answer = [Int]()
+//    
+//    for i in 1...n {
+//        if n % i == 0 {
+//            answer.append(i)
+//        }
+//    }
+//    
+//    return answer
+//}
