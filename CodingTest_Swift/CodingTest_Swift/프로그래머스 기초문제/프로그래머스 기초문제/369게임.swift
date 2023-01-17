4f77139a7306219fba1caab46ff0eb0a98324d85
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2023-01-17 15:06:14 +0900

    [Clear] 대문자와 소문자 - 2023/01/17

--- CodingTest_Swift/CodingTest_Swift/프로그래머스 기초문제/프로그래머스 기초문제/369게임.swift
+++ CodingTest_Swift/CodingTest_Swift/프로그래머스 기초문제/프로그래머스 기초문제/369게임.swift
@@ -5,14 +5,14 @@
 //  Created by 이창형 on 2023/01/17.
 //
 
-import Foundation
-
-func solution(_ order:Int) -> Int {
-    var count = 0
-    for i in String(order) {
-        if Int(String(i)) == 3 || Int(String(i)) == 6 || Int(String(i)) == 9 {
-            count += 1
-        }
-    }
-    return count
-}
+//import Foundation
+//
+//func solution(_ order:Int) -> Int {
+//    var count = 0
+//    for i in String(order) {
+//        if Int(String(i)) == 3 || Int(String(i)) == 6 || Int(String(i)) == 9 {
+//            count += 1
+//        }
+//    }
+//    return count
+//}
---
+++ CodingTest_Swift/CodingTest_Swift/프로그래머스 기초문제/프로그래머스 기초문제/대문자와 소문자.swift
@@ -0,0 +1,22 @@
+//
+//  대문자와 소문자.swift
+//  CodingTest_Swift
+//
+//  Created by 이창형 on 2023/01/17.
+//
+
+//import Foundation
+//
+//func solution(_ my_string:String) -> String {
+//    var answer = ""
+//    
+//    for i in my_string {
+//        var word = String(i)
+//        var up = word.uppercased()
+//        var low = word.lowercased()
+//        
+//        answer += (word == up) ? low : up
+//    }
+//    
+//    return answer
+//}
