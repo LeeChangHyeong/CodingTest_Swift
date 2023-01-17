e754cd5a8aaae0210264295b341071eb822f52e8
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2023-01-17 14:19:15 +0900

    [Clear] 가까운 수 - 2022/01/17

---
+++ CodingTest_Swift/CodingTest_Swift/프로그래머스 기초문제/프로그래머스 기초문제/가까운 수.swift
@@ -0,0 +1,28 @@
+//
+//  가까운 수.swift
+//  CodingTest_Swift
+//
+//  Created by 이창형 on 2023/01/17.
+//
+
+import Foundation
+
+func solution(_ array:[Int], _ n:Int) -> Int {
+    var num = 100
+    var answer = 0
+    
+    for i in array.sorted() {
+        if n - i < 0 {
+            if -(n - i) < num {
+                num = -(n - i)
+                answer = i
+            }
+        } else {
+            if (n - i) < num {
+                num = (n - i)
+                answer = i
+            }
+        }
+    }
+    return answer
+}
--- CodingTest_Swift/CodingTest_Swift/프로그래머스 기초문제/프로그래머스 기초문제/암호 해독.swift
+++ CodingTest_Swift/CodingTest_Swift/프로그래머스 기초문제/프로그래머스 기초문제/암호 해독.swift
@@ -5,19 +5,19 @@
 //  Created by 이창형 on 2023/01/14.
 //
 
-import Foundation
-
-func solution(_ cipher:String, _ code:Int) -> String {
-    var count = 0
-    var answer = ""
-    
-    for i in cipher {
-        count += 1
-        if count == code {
-              print(i)
-            answer.append(i)
-            count = 0
-        }
-    }
-    return answer
-}
+//import Foundation
+//
+//func solution(_ cipher:String, _ code:Int) -> String {
+//    var count = 0
+//    var answer = ""
+//    
+//    for i in cipher {
+//        count += 1
+//        if count == code {
+//              print(i)
+//            answer.append(i)
+//            count = 0
+//        }
+//    }
+//    return answer
+//}
