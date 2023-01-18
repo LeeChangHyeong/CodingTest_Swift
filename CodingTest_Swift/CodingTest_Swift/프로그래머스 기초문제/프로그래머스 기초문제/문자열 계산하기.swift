7790b0c2a34d435287d767f915f1823916cf393d
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2023-01-18 14:30:47 +0900

    [Clear] 문자열 계산하기 - 2023/01/18

--- CodingTest_Swift/CodingTest_Swift/프로그래머스 기초문제/프로그래머스 기초문제/문자열 계산하기.swift
+++ CodingTest_Swift/CodingTest_Swift/프로그래머스 기초문제/프로그래머스 기초문제/문자열 계산하기.swift
@@ -5,18 +5,18 @@
 //  Created by 이창형 on 2023/01/18.
 //
 
-import Foundation
-
-func solution(_ my_string:String) -> Int {
-    var a = my_string.split(separator: " ")
-    var result = Int(a[0])!
-    for i in a.enumerated() {
-        if i.element == "+" {
-            result += Int(a[i.offset + 1])!
-        }
-        if i.element == "-" {
-            result -= Int(a[i.offset + 1])!
-        }
-    }
-    return result
-}
+//import Foundation
+//
+//func solution(_ my_string:String) -> Int {
+//    var a = my_string.split(separator: " ")
+//    var result = Int(a[0])!
+//    for i in a.enumerated() {
+//        if i.element == "+" {
+//            result += Int(a[i.offset + 1])!
+//        }
+//        if i.element == "-" {
+//            result -= Int(a[i.offset + 1])!
+//        }
+//    }
+//    return result
+//}
---
+++ CodingTest_Swift/CodingTest_Swift/프로그래머스 기초문제/프로그래머스 기초문제/숫자 찾기.swift
@@ -0,0 +1,22 @@
+//
+//  숫자 찾기.swift
+//  CodingTest_Swift
+//
+//  Created by 이창형 on 2023/01/18.
+//
+
+//import Foundation
+//
+//func solution(_ num:Int, _ k:Int) -> Int {
+//    var count = 1
+//    for i in String(num) {
+//        if Int(String(i)) == k {
+//            return count
+//        } else if count == String(num).count {
+//            return -1
+//        } else {
+//            count += 1
+//        }
+//    }
+//    return 0
+//}
