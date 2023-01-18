fd13651b27a9c2ae0114eb811ad01df44a91bb56
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2023-01-18 12:25:55 +0900

    [Clear] 가장 큰 수 찾기 - 2022/01/18

---
+++ CodingTest_Swift/CodingTest_Swift/프로그래머스 기초문제/프로그래머스 기초문제/가장 큰 수 찾기.swift
@@ -0,0 +1,15 @@
+//
+//  가장 큰 수 찾기.swift
+//  CodingTest_Swift
+//
+//  Created by 이창형 on 2023/01/18.
+//
+
+//import Foundation
+//
+//func solution(_ array:[Int]) -> [Int] {
+//    var answer = [Int]()
+//    answer.append(array.max() ?? 0)
+//    answer.append(array.firstIndex(of: array.max() ?? 0) ?? 0)
+//    return answer
+//}
