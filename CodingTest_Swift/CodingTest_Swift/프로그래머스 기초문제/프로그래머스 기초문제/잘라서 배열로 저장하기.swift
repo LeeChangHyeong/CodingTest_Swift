3d3a643fa57947f201f51916adffccf829be1c51
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2023-01-20 14:08:41 +0900

    [Clear] 잘라서 배열로 저장하기 - 2023/01/20

---
+++ CodingTest_Swift/CodingTest_Swift/프로그래머스 기초문제/프로그래머스 기초문제/잘라서 배열로 저장하기.swift
@@ -0,0 +1,25 @@
+//
+//  잘라서 배열로 저장하기.swift
+//  CodingTest_Swift
+//
+//  Created by 이창형 on 2023/01/20.
+//
+
+//import Foundation
+//
+//func solution(_ my_str:String, _ n:Int) -> [String] {
+//    var result: [String] = []
+//    var answer = ""
+//
+//    for s in my_str {
+//        answer += String(s)
+//        if answer.count == n {
+//            result.append(answer)
+//            answer = ""
+//        }
+//    }
+//    if !answer.isEmpty {
+//        result.append(answer)
+//    }
+//    return result
+//}
