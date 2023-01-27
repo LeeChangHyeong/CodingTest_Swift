e63751c887181ce020c129df79e3b0c9b4026998
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2023-01-27 14:01:49 +0900

    [Clear] 다항식 더하기 - 2023/01/27

---
+++ CodingTest_Swift/CodingTest_Swift/프로그래머스 기초문제/프로그래머스 기초문제/다항식 더하기.swift
@@ -0,0 +1,33 @@
+//
+//  다항식 더하기.swift
+//  CodingTest_Swift
+//
+//  Created by 이창형 on 2023/01/27.
+//
+
+import Foundation
+
+func solution(_ polynomial:String) -> String {
+    var a = polynomial.components(separatedBy: " + ")
+    var b = 0
+    var c = 0
+    
+    for i in a {
+        if i.contains("x") {
+            if i == "x" {
+                b += 1
+            } else {
+                var temp = i
+                temp.removeLast()
+                b += Int(temp)!
+            }
+        } else {
+            c += Int(i)!
+        }
+    }
+    
+    if b == 0 && c == 0 { return "0" }
+    if b == 0 { return  "\(c)" }
+    if c == 0 { if b != 1 { return "\(b)x" } else { return "x" } }
+    return b == 1 ? "x + \(c)" : "\(b)x + \(c)"
+}
--- CodingTest_Swift/CodingTest_Swift/프로그래머스 기초문제/프로그래머스 기초문제/최댓값 만들기(2).swift
+++ CodingTest_Swift/CodingTest_Swift/프로그래머스 기초문제/프로그래머스 기초문제/최댓값 만들기(2).swift
@@ -5,17 +5,17 @@
 //  Created by 이창형 on 2023/01/26.
 //
 
-import Foundation
-
-func solution(_ numbers:[Int]) -> Int {
-    var count = numbers.count
-    var sortedNum = numbers.sorted(by: >)
-    var num = sortedNum[count - 1] * sortedNum[count - 2]
-    var num2 = sortedNum[0] * sortedNum[1]
-    
-    if num > num2 {
-        return num
-    } else {
-        return num2
-    }
-}
+//import Foundation
+//
+//func solution(_ numbers:[Int]) -> Int {
+//    var count = numbers.count
+//    var sortedNum = numbers.sorted(by: >)
+//    var num = sortedNum[count - 1] * sortedNum[count - 2]
+//    var num2 = sortedNum[0] * sortedNum[1]
+//    
+//    if num > num2 {
+//        return num
+//    } else {
+//        return num2
+//    }
+//}
