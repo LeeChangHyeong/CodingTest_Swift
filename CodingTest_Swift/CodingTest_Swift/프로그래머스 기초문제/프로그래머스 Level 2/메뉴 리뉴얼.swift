8923d0af8d3374592b95e63420b4e979cfe52e9a
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2023-05-08 10:46:55 +0900

    2023/05/08

---
+++ CodingTest_Swift/CodingTest_Swift/프로그래머스 기초문제/프로그래머스 Level 2/메뉴 리뉴얼.swift
@@ -0,0 +1,26 @@
+//
+//  메뉴 리뉴얼.swift
+//  CodingTest_Swift
+//
+//  Created by 이창형 on 2023/05/05.
+//
+
+//import Foundation
+//
+//func solution(_ orders:[String], _ course:[Int]) -> [String] {
+//    var dic = [String:Int]()
+//    
+//    func combi(_ order: [String]) {
+//        for i in course {
+//            
+//        }
+//    }
+//    
+//    for i in orders {
+//        var order = i.map{String($0)}
+//        combi(order)
+//    }
+//    
+//    return []
+//}
+//
