f9916e536e91e5b3b607bd5b5b61ff4ecef0ddea
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2023-05-08 15:05:41 +0900

    2023/05/08

--- CodingTest_Swift/CodingTest_Swift/프로그래머스 기초문제/프로그래머스 Level 2/두 큐 합 같게 만들기.swift
+++ CodingTest_Swift/CodingTest_Swift/프로그래머스 기초문제/프로그래머스 Level 2/두 큐 합 같게 만들기.swift
@@ -11,23 +11,36 @@ func solution(_ queue:[Int], _ queue2:[Int]) -> Int {
     var sum = queue.reduce(0) {$0+$1}
     var sum2 = queue2.reduce(0) {$0+$1}
     var totalSum = sum + sum2
-    var answer = -1
-    var queue = queue
-    var queue2 = queue2
-    var odd = 0
+    var answer = 0
+    var left = 0
+    var right = queue.count
+    var array = queue + queue2
+    var goal = (totalSum / 2)
     
     if totalSum % 2 != 0 {
-        return answer
+        return -1
     }
     
-    while sum != sum2 {
-        if sum > sum2 {
-            queue2.append(queue.removeFirst())
+    if goal < queue.max()! || goal < queue2.max()! {
+        return -1
+    }
+    
+    while right < array.count && left <= right {
+        if sum < goal {
+            sum += array[right]
+            right += 1
+        } else if sum > goal {
+            sum -= array[left]
+            left += 1
+        } else {
+            break
         }
-        
+        answer += 1
     }
     
-    
+    if sum != goal {
+        return -1
+    }
 
-   return -2
+   return answer
 }
