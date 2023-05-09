5898affa2130456aacc3eddbb14f3709811e3ab0
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2023-05-09 12:47:42 +0900

    2023/05/09 단어 변환 bfs

--- CodingTest_Swift/CodingTest_Swift/프로그래머스 기초문제/프로그래머스 Level 3/네트워크.swift
+++ CodingTest_Swift/CodingTest_Swift/프로그래머스 기초문제/프로그래머스 Level 3/네트워크.swift
@@ -5,27 +5,27 @@
 //  Created by 이창형 on 2023/05/09.
 //
 
-import Foundation
-
-func solution(_ n:Int, _ computers:[[Int]]) -> Int {
-    var check = Array(repeating: false, count: n)
-    var answer = 0
-    
-    func dfs(_ x: Int) {
-        check[x] = true
-        for i in 0..<n {
-            if computers[x][i] == 1 && check[i] == false {
-                dfs(i)
-            }
-        }
-    }
-    
-    for i in 0..<n {
-        if check[i] == false {
-            answer += 1
-            dfs(i)
-        }
-    }
-    
-    return answer
-}
+//import Foundation
+//
+//func solution(_ n:Int, _ computers:[[Int]]) -> Int {
+//    var check = Array(repeating: false, count: n)
+//    var answer = 0
+//    
+//    func dfs(_ x: Int) {
+//        check[x] = true
+//        for i in 0..<n {
+//            if computers[x][i] == 1 && check[i] == false {
+//                dfs(i)
+//            }
+//        }
+//    }
+//    
+//    for i in 0..<n {
+//        if check[i] == false {
+//            answer += 1
+//            dfs(i)
+//        }
+//    }
+//    
+//    return answer
+//}
---
+++ CodingTest_Swift/CodingTest_Swift/프로그래머스 기초문제/프로그래머스 Level 3/단어변환.swift
@@ -0,0 +1,56 @@
+//
+//  단어변환.swift
+//  CodingTest_Swift
+//
+//  Created by 이창형 on 2023/05/09.
+//
+
+import Foundation
+
+func solution(_ begin:String, _ target:String, _ words:[String]) -> Int {
+    var answerStr = target.map{String($0)}
+    var visited = Array(repeating: false, count: words.count)
+    var count = 0
+    var queue = [(String, Int)]()
+    
+    queue.append((begin,0))
+    
+    if !words.contains(target) {
+        return 0
+    }
+    
+    func check(_ str: String, _ str2: String) -> Bool {
+        var a = str.map{String($0)}
+        var b = str2.map{String($0)}
+        var count = 0
+        
+        for i in 0..<a.count {
+            if a[i] == b[i] {
+                count += 1
+            }
+        }
+        
+        if count == 2 {
+            return true
+        }
+    
+        return false
+    }
+    
+    while !queue.isEmpty {
+        var now = queue.removeFirst()
+        
+        if now.0 == target {
+            return now.1
+        }
+        
+        for i in 0..<words.count {
+            if check(words[i], now.0) && !visited[i] {
+                visited[i] = true
+                queue.append((words[i], now.1 + 1))
+            }
+        }
+    }
+    
+    return 0
+}
