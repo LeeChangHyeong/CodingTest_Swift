da956b0b915e673ab7ac21dd6725e5bc5dbd51df
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2023-05-09 14:56:11 +0900

    2023/05/09

---
+++ CodingTest_Swift/CodingTest_Swift/프로그래머스 기초문제/프로그래머스 Level 1/과일 장수.swift
@@ -0,0 +1,24 @@
+//
+//  과일 장수.swift
+//  CodingTest_Swift
+//
+//  Created by 이창형 on 2023/05/09.
+//
+
+//import Foundation
+//
+//func solution(_ k:Int, _ m:Int, _ score:[Int]) -> Int {
+//    var score = score.sorted(by: >)
+//    var answer = 0
+//    var array = [Int]()
+//
+//    for i in score {
+//        array.append(i)
+//        if array.count == m {
+//            answer += array.min()! * m
+//            array.removeAll()
+//        }
+//    }
+//
+//    return answer
+//}
--- CodingTest_Swift/CodingTest_Swift/프로그래머스 기초문제/프로그래머스 Level 1/명예의 전당(1).swift
+++ CodingTest_Swift/CodingTest_Swift/프로그래머스 기초문제/프로그래머스 Level 1/명예의 전당(1).swift
@@ -5,33 +5,33 @@
 //  Created by 이창형 on 2023/05/09.
 //
 
-import Foundation
-
-func solution(_ k:Int, _ score:[Int]) -> [Int] {
-    var array = [Int]()
-    var answer = [Int]()
-    var score = score
-    var count = k - score.count
-    
-    if count > 0 {
-        for i in 0..<score.count {
-        array.append(score[i])
-        answer.append(array.min()!)
-    }
-    } else {
-        for i in 0..<k {
-        array.append(score[i])
-        answer.append(array.min()!)
-    }
-    
-    for i in k..<score.count {
-        array.append(score[i])
-        var a = array.sorted(by: <)
-        a.removeFirst()
-        answer.append(a[0])
-        array = a
-        }
-    }
-
-    return answer
-}
+//import Foundation
+//
+//func solution(_ k:Int, _ score:[Int]) -> [Int] {
+//    var array = [Int]()
+//    var answer = [Int]()
+//    var score = score
+//    var count = k - score.count
+//    
+//    if count > 0 {
+//        for i in 0..<score.count {
+//        array.append(score[i])
+//        answer.append(array.min()!)
+//    }
+//    } else {
+//        for i in 0..<k {
+//        array.append(score[i])
+//        answer.append(array.min()!)
+//    }
+//    
+//    for i in k..<score.count {
+//        array.append(score[i])
+//        var a = array.sorted(by: <)
+//        a.removeFirst()
+//        answer.append(a[0])
+//        array = a
+//        }
+//    }
+//
+//    return answer
+//}
