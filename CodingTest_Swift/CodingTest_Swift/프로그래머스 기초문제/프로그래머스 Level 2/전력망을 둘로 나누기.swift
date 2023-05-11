4085563a37a50ffcebec4e8c5c1aa6690bee9ddd
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2023-05-11 15:57:32 +0900

    20230511

--- CodingTest_Swift/CodingTest_Swift/프로그래머스 기초문제/프로그래머스 Level 2/전력망을 둘로 나누기.swift
+++ CodingTest_Swift/CodingTest_Swift/프로그래머스 기초문제/프로그래머스 Level 2/전력망을 둘로 나누기.swift
@@ -9,38 +9,38 @@
 //
 //func solution(_ n:Int, _ wires:[[Int]]) -> Int {
 //    var route = [Int:[Int]]()
-//    
+//
 //    for wire in wires {
 //        if route[wire[0]] != nil {
 //            route[wire[0]]?.append(wire[1])
 //        } else {
 //            route[wire[0]] = [wire[1]]
 //        }
-//        
+//
 //        if route[wire[1]] != nil {
 //            route[wire[1]]?.append(wire[0])
 //        } else {
 //            route[wire[1]] = [wire[0]]
 //        }
 //    }
-//    
+//
 //    var result = n
 //    for i in route {
 //        for j in i.value {
 //            // 전력망 끊기
 //            route[i.key] = route[i.key]!.filter{$0 != j}
 //            route[j] = route[j]!.filter{$0 != i.key}
-//            
+//
 //            let a = abs(bfs(route, i.key).count - bfs(route, j).count)
-//            
+//
 //            result = a < result ? a : result
-//            
+//
 //            // 전력망 복구
 //            route[i.key]?.append(j)
 //            route[j]?.append(i.key)
 //        }
 //    }
-//    
+//
 //    return result
 //}
 //
@@ -48,7 +48,7 @@
 //    var visited = [Int]()
 //    var needVisit = [Int]()
 //    needVisit.append(node)
-//    
+//
 //    while !needVisit.isEmpty {
 //        let now = needVisit.removeFirst()
 //        if visited.contains(now) {
@@ -57,6 +57,6 @@
 //        visited.append(now)
 //        needVisit.append(contentsOf: route[now] ?? [])
 //    }
-//    
+//
 //    return visited
 //}
