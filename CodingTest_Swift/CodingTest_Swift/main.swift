3ca25b59a0776f13cb50a98dd4c5726fa468019d
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2023-05-29 18:47:29 +0900

    20230529

--- CodingTest_Swift/CodingTest_Swift/main.swift
+++ CodingTest_Swift/CodingTest_Swift/main.swift
@@ -1,1 +1,14 @@
+var words = [[String]](repeating: [String](repeating: "", count: 15), count: 5)
 
+for i in 0..<5 {
+    let word = readLine()!.map { String($0) }
+    let lastIndex = word.count - 1
+    // (인덱스 범위 만큼, word로 바꿔준다)
+    words[i].replaceSubrange(0..<lastIndex, with: word)
+}
+
+for i in 0..<15 {
+    for j in 0..<5 {
+        print(words[j][i], terminator: "")
+    }
+}
